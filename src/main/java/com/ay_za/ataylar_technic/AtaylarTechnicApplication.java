2439fd0d6aa1665117f0900e6755ecab1a477d88
zaferakgul <akglzfr@gmail.com>
2025-06-14 13:44:42 +0300

    first commit

---
+++ src/main/java/com/ay_za/ataylar_technic/AtaylarTechnicApplication.java
@@ -0,0 +1,13 @@
+package com.ay_za.ataylar_technic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtaylarTechnicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtaylarTechnicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ay_za/ataylar_technic/AtaylarTechnicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ay_za.ataylar_technic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtaylarTechnicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
